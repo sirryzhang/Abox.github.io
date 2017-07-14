2c7fefc63f75fc47f75d2a40717a3947e60931a7
sirryzhang <siyu@cca.edu>
2017-07-14 12:54:11 -0700

    2

--- main.js
+++ main.js
@@ -1,47 +1,96 @@
-
+var scene,camera,box, renderer, mesh, geometry, material;
 function init(){
-  var scene=new THREE.Scene();
+   scene=new THREE.Scene();
+   var gui  = new dat.GUI();
 
-  var box = getBox(1,1,1);
+   box = getBox(6,6,6);
+   plane  = getPlane(150);
+   var pointLight  = getPointLight(1);
+   var sphere=getSphere(0.5);
+
+
+  gui.add(pointLight,'intensity',0,10);
+  gui.add(pointLight.position,'y',-10,10);
 
   scene.add(box);
+  scene.add(plane);
+  scene.add(pointLight);
+  pointLight.add(sphere);
 
-  var camera  = new THREE.PerspectiveCamera(
+  // scene.fog=new THREE.FogExp2(0xffffff, 0.01);
+
+   camera  = new THREE.PerspectiveCamera(
     45,
     window.innerWidth/window.innerHeight,
     1,
     10000
   );
-  camera.position.z = 1000;
+  camera.position.z = 100;
+  // pointLight.position.z = 10;
+  // pointLight.position.y=10;
+  pointLight.intensity=2;
+  plane.position.z= -20;
 
-
-  var renderer=new THREE.WebGLRenderer();
+   renderer=new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0xffffff);
   document.getElementById('webgl').appendChild(renderer.domElement);
   renderer.render(
     scene,
     camera
   );
-  animation();
+  return scene;
 };
 
 function getBox(w,h,d){
-  var geometry  = new THREE.BoxGeometry(w,h,d);
-  var material  = new THREE.MeshBasicMaterial({
-    color:0x00ff00,
+   geometry  = new THREE.BoxGeometry(w,h,d);
+   material  = new THREE.MeshPhongMaterial({
+    color:0x000000,
     wireframe:true
   });
-  var mesh  = new THREE.Mesh(
+   mesh  = new THREE.Mesh(
+    geometry,
+    material
+  );
+  return mesh;
+};
+
+function getSphere(size){
+   geometry  = new THREE.SphereGeometry(size,24,24);
+   material  = new THREE.MeshBasicMaterial({
+    color:0xffffff,
+  });
+   mesh  = new THREE.Mesh(
+    geometry,
+    material
+  );
+  return mesh;
+};
+
+function getPlane(size){
+   geometry  = new THREE.PlaneGeometry(size,size);
+   material  = new THREE.MeshPhongMaterial({
+    color:  0x000000,
+  });
+   mesh  = new THREE.Mesh(
     geometry,
     material
   );
   return mesh;
 }
+
 function animate(){
   requestAnimationFrame(animate);
 
-  mesh.rotation.x +_1;
-  mesh.rotation.y +_1;
+  box.rotation.x +=0.01;
+  box.rotation.y +=0.01;
 
   renderer.render(scene,camera);
+};
+
+function getPointLight(intensity){
+  var light=new THREE.PointLight(0xffffff,intensity);
+  return light;
 }
+init();
+animate();
